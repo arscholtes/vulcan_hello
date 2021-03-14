1f45527c4120e53980b8a2b0f290a459790d298f
Th3MFG <arscholtes@gmail.com>
2021-03-14 19:25:10 -0400

    Validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates_presence_of :name , presence: true
+  validates_presence_of :email , presence: true
+  validates :email, confirmation: true
 end
